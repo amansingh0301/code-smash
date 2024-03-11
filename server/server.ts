dc19723f369c901c415702a11490ee576d1c784a
as9997450@gmail.com <as9997450@gmail.com>
2024-03-11 16:59:15 +0530

    storing config

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,20 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 
 app.use('/',express.static('build'));
 
+app.get('/user',(req,res) => {
+    const data = fetch(`${process.env.jsonurl}`)
+      .then(response => response.json())
+      res.send(data);
+})
+
 app.get('/health',(req,res) => {
-    res.send('OK');
+      res.send('OK');
 })
 
 const port = process.env.PORT || 8080;
 app.listen(port,()=>{
-    console.log('server-started');
+    console.log(`server-started ${port}`);
 })
