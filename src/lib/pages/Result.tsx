9a9e90d27fce2f2202c9ea6cba739f83d9d7b7b3
as9997450@gmail.com <as9997450@gmail.com>
2024-03-24 21:54:03 +0530

    result page created and populated and designed

--- src/lib/pages/Result.tsx
+++ src/lib/pages/Result.tsx
@@ -3,10 +3,75 @@ import { useSelector } from 'react-redux';
 import { InitialState } from '../../store/initialState';
 export function Result() {
     const currentPath = window.location.pathname;
+    const questionsList = useSelector((state: InitialState) => state.contest.questionsList);
     const result = useSelector((state: InitialState) => state.contest.result);
     return (
         <div className='result slide-in-right'>
-            correct: {result.result.correct} un-attempted: {result.result.unAttempted} incorrect: {result.result.inCorrect} 
+            <div className='analytics'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                Total Questions
+                            </th>
+                            <th className='noOfQuestionsCorrect'>
+                                Correct
+                            </th>
+                            <th>
+                                Unaatempted
+                            </th>
+                            <th className='noOfQuestionsWrong'>
+                                Wrong
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{result.result.correct + result.result.unAttempted + result.result.inCorrect}</td>
+                            <td className='noOfQuestionsCorrect'>{result.result.correct}</td>
+                            <td>{result.result.unAttempted}</td>
+                            <td className='noOfQuestionsWrong'>{result.result.inCorrect}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className='questionsResponse slide-in-right'>
+                {
+                    questionsList.map((questionId, index) => {
+                        return result.questions.filter(questionDetail => questionDetail._id === questionId).map(
+                            question => {
+                                return (
+                                    <>
+                                        <div className='questionLine slide-in-right'>
+                                            Q{index+1}. {question.question}
+                                        </div>
+                                        <div className='options slide-in-right'>
+                                            {
+                                                question.options.map(option => {
+                                                    return (
+                                                        <div className={`questionOptions ${question.answer === option ? 'correctOption' : ''} ${ question.marked !== question.answer && question.marked === option ? 'choseWrong' : ''} ${question.marked === '' && question.answer !== option ? 'unattempted' : ''}`}>
+                                                            {option}
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                        <div className='explanation'>
+                                            {
+                                               question.explanation && 
+                                               <>
+                                                    <div>Explanation: </div>
+                                                    <div>{question.explanation}</div>
+                                               </>
+                                            }
+                                        </div>
+                                    </>
+                                )
+                            }
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
