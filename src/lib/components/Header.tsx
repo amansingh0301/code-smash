6486a1dc7f847a260503f6bc7b53ca9d5beaf8b0
as9997450@gmail.com <as9997450@gmail.com>
2024-03-24 22:49:21 +0530

    navigating to home page added

--- src/lib/components/Header.tsx
+++ src/lib/components/Header.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 export function Header() {
+    const navigate = useNavigate();
+
+    const goToHome = () => {
+        navigate('/');
+    }
+
     return (
         <div className='header'>
-            <div className='logo'>
+            <div className='logo' onClick={goToHome}>
                 <div className='logo__image'></div>
             </div>
         </div>
